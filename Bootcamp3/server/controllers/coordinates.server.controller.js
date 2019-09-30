ebe6ff56434c3867ef9107140d97d4842411cc8a
danielgreg2 <gdecanio@ufl.edu>
2019-09-30 18:12:06 -0400

    Cleaned up how forward geocoding is performed

--- Bootcamp3/server/controllers/coordinates.server.controller.js
+++ Bootcamp3/server/controllers/coordinates.server.controller.js
@@ -24,40 +24,17 @@ module.exports = function(req, res, next) {
       url: 'https://api.opencagedata.com/geocode/v1/json', 
       qs: options
       }, function(error, response, body) {
-		opencage.geocode({q: options.q, key: options.key}).then(data => {
-			//console.log(JSON.stringify(data));
-			//For ideas about response and error processing see https://opencagedata.com/tutorials/geocode-in-nodejs
-				if (data.status.code == 200) {
-					//Below is just debugging that prints to console the area and coordinates of address
-					if (data.results.length > 0) {
-					  var place = data.results[0];
-					  console.log(place.formatted);
-					  console.log(place.geometry);
-					}
-				} else if (data.status.code == 402) {
-					//This error occurs if I have used too many free trials of the API
-					console.log('hit free-trial daily limit');
-					console.log('become a customer: https://opencagedata.com/pricing'); 
-				} else {
-					// other possible response codes:
-					// https://opencagedata.com/api#codes
-					console.log('error', data.status.message);
-				}
-			//Saves the coordinates of the address into "req.results"
-			req.results = data.results[0].geometry;
-			next();
-			}).catch(error => {
-				console.log('error', error.message);
-			});
-        
-        //JSON.parse to get contents. Remember to look at the response's JSON format in open cage data
-        
+			//JSON.parse to get contents. Remember to look at the response's JSON format in open cage data
+			body = JSON.parse(body);
+                
         /*Save the coordinates in req.results -> 
           this information will be accessed by listings.server.model.js 
           to add the coordinates to the listing request to be saved to the database.
 
           Assumption: if we get a result we will take the coordinates from the first result returned
         */
+        	req.results = body.results[0].geometry;
+			next();
     });
   } else {
     next();
